89db7a34c683d39a13fa3d33d7b6964676906458
panpan <panpan@panpandeMac-mini.local>
2020-10-23 18:35:52 +0800

    fix 滑动问题

---
+++ src/view/scroll/index.js
@@ -0,0 +1,236 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState , useEffect, useRef} from 'react';
+import {
+  Platform,
+  Dimensions,
+  SafeAreaView,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  PanResponder,
+  TextInput,
+  StyleSheet,
+  SectionList,
+  ScrollView,
+  FlatList,
+  Alert,
+  View,
+  Button,
+  Image,
+  ImageBackground,
+  Animated,
+  Text,
+  StatusBar,
+} from 'react-native';
+import Video from 'react-native-video';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const {width, height, scale} = Dimensions.get('window');
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
+
+const Scroll = () => {
+  const [text, setText] = useState('')
+  const [selectedId, setSelectedId] = useState(null);
+  const [play, setPlay] = useState(true);
+  const [current, setCurrent] = useState(0);
+  const currentRef = useRef(0);
+  const videoPlayRef = useRef([]);
+
+  const flatRef = useRef();
+  const startTimestampRef = useRef();
+  const videoRef =[{
+    uri: require('../../static/1.mp4')
+  }, {
+    uri: require('../../static/2.mp4')
+  },{
+    uri: require('../../static/3.mp4')
+  }]
+  
+  const panResponder = useRef(
+    PanResponder.create({
+      // 要求成为响应者：
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onStartShouldSetPanResponderCapture: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponderCapture: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        // 滑动开始，记录时间戳
+        startTimestampRef.current = evt.nativeEvent.timestamp
+      },
+      onPanResponderMove: (evt, gestureState) => {
+        // 累计滑动纵向距离
+        let y = gestureState.dy
+        console.log('----current' + currentRef.current +  'time----' + evt.nativeEvent.timestamp)
+        console.log('----moving' + (currentRef.current * height - y))
+        flatRef.current.scrollToOffset({
+          offset: currentRef.current * height - y,
+          animated: false
+        }) 
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        // 滑动结束时间戳
+        let endTimestamp = evt.nativeEvent.timestamp
+        // 滑动距离，根据滑动距离与时间戳计算是否切换到下一个条目
+        let y = gestureState.dy
+        console.log('----moveend' + y)
+        let speed = y / (endTimestamp - startTimestampRef.current)
+        console.log('touchtime---' + (endTimestamp - startTimestampRef.current))
+        console.log('speed || ' + speed) 
+        if ((endTimestamp - startTimestampRef.current < 100) && Math.abs(speed) < 0.2) {
+            return changePlay()
+        }
+        if ((y > 100 || speed > 0.5) && currentRef.current > 0) {
+          // 上一夜
+          goIndex(currentRef.current - 1, true)
+        } else if ((y < -100 || speed < -0.5)&& currentRef.current < 2) {
+          // 下一页
+          goIndex(currentRef.current + 1, true)
+        } else {
+          //停留本页
+          goIndex(currentRef.current, false)
+        }
+      }
+    })
+  ).current;
+  // 跳转index
+  const goIndex = (index, flag) => {
+    console.log('index' + index)
+    flatRef.current.scrollToIndex({index, animated: true})
+    currentRef.current = index
+    setCurrent(index)
+    // 翻视频
+    if (flag) {
+      videoPlayRef.current[index].current.seek(0)
+      setPlay(true)
+    }
+  }
+  
+  
+
+  // 滚动
+  const handelonScroll = (e) => {
+    // 滚动偏移量
+    console.log('scoll' + e.nativeEvent.contentOffset.y)
+  }
+  // 改变播放状态
+  changePlay = () => {
+    console.log('---play---')
+    setPlay(!play)
+  }
+
+  const videoError = () => {
+    console.log('video---error')
+  }
+  const onBuffer = () => {
+    console.log('video---onBuffer')
+  }
+
+  const renderItem = ({ item, index }) => {
+    const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+    videoPlayRef.current[index] = React.createRef()
+    return (
+      <TouchableWithoutFeedback   onPress={changePlay} >
+        <View style={[styles.item, {backgroundColor}]}>
+          <Text style={styles.title}>{index} | {item.title} {selectedId}</Text>
+          <Video 
+            ref={videoPlayRef.current[index]}
+            source={videoRef[index].uri} 
+            style={styles.backgroundVideo}
+            resizeMode="contain" 
+            paused={!((index === current) && play)}
+            repeat={true}      
+          />
+          {
+            !play?
+              <Ionicons name={'ios-play'} size={34} color={'#fff'} style={styles.iconPlay}/>
+            : null
+          }
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  };
+
+  const _onLongPressButton = () => {
+    Alert.alert('You long-pressed the button!')
+  }
+
+  return (
+      <FlatList
+        {...panResponder.panHandlers}
+        style={styles.container}
+        onMomentumScrollEnd={handelonScroll}
+        ref={flatRef}
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        extraData={selectedId}
+        scrollEnabled={false}
+      />
+  );
+};
+
+const styles = StyleSheet.create({
+  contain: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  },
+  scrollView: {
+    flex: 1,
+    height: height,
+    width: width,
+    backgroundColor: 'green',
+  },
+  container: {
+    flex: 1,
+    width: width,
+  },
+  item: {
+    padding: 0,
+    margin: 0,
+    flex: 1,
+    height: height,
+    width: width,
+    position: 'relative',
+  },
+  backgroundVideo: {
+    width: width,
+    height: height,
+  }, 
+  title: {
+    fontSize: 32,
+    position: 'absolute',
+    top: 12,
+    left: width/2 - 6,
+    zIndex: 5000
+  },
+  iconPlay: {
+    position: 'absolute',
+    top: height/2,
+    left: width/2 - 6
+  }
+});
+
+export default Scroll;
